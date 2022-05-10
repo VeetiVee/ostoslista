727c026937c816eb222a4bb0001eedc54a7687dc
VeetiVee <veeti.vainikka@gmail.com>
2022-05-10 15:54:35 +0300

    push

--- App.js
+++ App.js
@@ -1,10 +1,39 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import React, { useState } from "react";
+import { StyleSheet, Text, View, TextInput, Button, FlatList } from "react-native";
 
 export default function App() {
+  const [groceries, setGroceries] = useState("");
+  const [list, setList] = useState([]);
+
+  const add = () => {
+    setList([...list, { key: groceries }]);
+    setGroceries("");
+  };
+
+  const clear = () => {
+    setList([]);
+  };
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+     
+      <TextInput
+        style={{ width: 200, borderColor: "gray", borderWidth: 1 }}
+        onChangeText={(text) => setGroceries(text)}
+        value={groceries}
+      />
+      <View style={{ flexDirection: "row" }}>
+        <Button onPress={add} title="Add" />
+        <Button onPress={clear} title="Clear" />
+      </View>
+      <Text>Shoppinglist</Text>
+      <FlatList
+        data={list}
+        renderItem={({ item }) => <Text>{item.key}</Text>}
+        keyExtractor={(item, index) => index.toString()}
+      />
+
       <StatusBar style="auto" />
     </View>
   );
@@ -13,8 +42,8 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
